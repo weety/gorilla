e8c4a2efd68903480064847ff2b18aa351998b2a
weety <luohui2320@gmail.com>
2018-10-27 10:16:07 +0800

    Add kalman filter sample code.

---
+++ gorilla/filter/kalman.h
@@ -0,0 +1,13 @@
+/*
+ * File      : kalman.h
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2018-10-26     weety    first version.
+ */
+
+#ifndef __KALMAN_H__
+#define __KALMAN_H__
+
+#endif
+
